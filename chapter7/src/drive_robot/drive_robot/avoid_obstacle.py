606773f9762d6f99ef6727c01e5a1777cc3ef470
Daviesss <ogunsdavis53@gmail.com>
2023-03-27 11:06:17 +0100

    ROS 2 nodes

--- chapter7/src/drive_robot/drive_robot/avoid_obstacle.py
+++ chapter7/src/drive_robot/drive_robot/avoid_obstacle.py
@@ -2,22 +2,20 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-# from nav_msgs.msg import Odometry
+
 
 class obstacle(Node):
     def __init__(self):
         super().__init__("avoidance_node")
         self.pub = self.create_publisher(Twist,'/cmd_vel',100)
         self.sub_laser = self.create_subscription(LaserScan,'/scan',self.callback_laser,100)
-        #self.sub_odometry = self.create_subscription(Odometry,'/odom',self.call_back_odom,100)
         self.velocity_message = Twist()
         self.turn_lefft = 0.2
         self.forward = 0.2
         self.stop = 0.0
         self.turn_right = 0.2
         
-        
-   #Drive the robot.
+
     def movement(self):
         print("The speed of the robot is",self.forward)
         self.velocity_message.linear.x = self.forward   
@@ -26,10 +24,10 @@ class obstacle(Node):
     def callback_laser(self,msg):
         self.get_logger().info('laser_scan ranges is: "%s"' % msg.ranges[100])
 
-        #let's make the robot keep driving forward,the robot stops when it senses an obstacle.
+      
         self.velocity_message.linear.x = self.forward
         self.pub.publish(self.velocity_message)
-        #check if there is an obstacle at the front of the robot,then sending stop to the robot.
+    
         for x in msg.ranges:
             if msg.ranges[100] < 1.88:
                 self.velocity_message.linear.x = self.stop
@@ -67,7 +65,6 @@ class obstacle(Node):
 def main(args=None):
     rclpy.init(args=args)
     store = obstacle()
-    #store.movement()
     rclpy.spin(store)
 
 
--- chapter7/src/drive_robot/drive_robot/move_sub.py
+++ chapter7/src/drive_robot/drive_robot/move_sub.py
@@ -1,20 +1,15 @@
 import rclpy 
 from geometry_msgs.msg import Twist 
 from rclpy.node import Node
-
-
-#class called sub,that takes in all the objects we need.
 class sub(Node):
     def __init__(self):
         super().__init__('move_robot_sub')
-        self.command_vel_sub = self.create_subscription(Twist,'/cmd_vel',self.sub_command_velocity,10) #passed in a call_back method that subscribes to the command velocity topic.
+        self.command_vel_sub = self.create_subscription(Twist,'/cmd_vel',self.sub_command_velocity,10) 
 
-    #subscriber to the linear velocity of x(when robot drives forward of linear of x)
     def sub_command_velocity(self,data):
-        self.message = data.linear.x  #store the data message of linear of x in an object called self.messages.
+        self.message = data.linear.x  
         self.get_logger().info('The robot drives forward, reading linear velocity of x ,which is: "%s"' %self.message) 
 
-#Main function
 def main(args=None):
     rclpy.init(args=args)
     sub_velocity = sub()
